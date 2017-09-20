e65d715985f4fd8607c90658ea3bc789c3102cbd
Patil <paabhije@amazon.com>
2017-09-20 21:13:55 +0530

    first serenity basic framework

---
+++ src/test/java/net/gmbh/solutions/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package net.gmbh.solutions.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import net.gmbh.solutions.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
